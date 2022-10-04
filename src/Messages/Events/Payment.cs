dfd7b16b4954a65492aa5d428e1152ce81b61013
berkalgl <berkalgull@gmail.com>
2022-10-04 19:04:34 +0300

    Messages and events add

---
+++ src/Messages/Events/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagesAndEvents.Events
+{
+    public class PaymentCompleted
+    {
+        public int OrderId { get; set; }
+    }
+    public class PaymentFailed
+    {
+        public int OrderId { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ src/Messages/Events/StockReserved.cs
@@ -0,0 +1,17 @@
+﻿namespace MessagesAndEvents.Events
+{
+    public class StockReserved
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public List<OrderItemMessage>? OrderItems { get; set; }
+    }
+
+    public class StockNotReserved
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string? Message { get; set; }
+    }
+}
