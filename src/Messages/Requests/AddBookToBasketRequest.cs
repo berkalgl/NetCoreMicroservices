8dbfe2b54166348df2ede87467ec25c62ccf2a63
berkalgl <berkalgull@gmail.com>
2022-10-05 16:05:26 +0300

    Messages and events done

---
+++ src/Messages/Requests/AddBookToBasketRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace MessagesAndEvents.Requests
+{
+    public class AddBookToBasketRequest
+    {
+        public int BookId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; } = 1;
+        public int UserId { get; set; }
+    }
+}
