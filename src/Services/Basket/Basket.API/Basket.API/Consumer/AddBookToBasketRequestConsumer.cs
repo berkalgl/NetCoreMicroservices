af3fd1287b20086c0c25a2b5733924996fbae8de
berkalgl <berkalgull@gmail.com>
2022-10-07 19:43:53 +0300

    Basket API

---
+++ src/Services/Basket/Basket.API/Basket.API/Consumer/AddBookToBasketRequestConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using MessagesAndEvents.Requests;
+
+namespace Basket.API.Consumer
+{
+    public class AddBookToBasketRequestConsumer : IConsumer<AddBookToBasketRequest>
+    {
+        private readonly ILogger _logger;
+        public AddBookToBasketRequestConsumer(ILogger<AddBookToBasketRequestConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<AddBookToBasketRequest> context)
+        {
+            _logger.LogInformation($"{context.Message.UserId} nolu kullanıcı; {context.Message.Name} isimli kitabı sepete ekledi");
+            //Here we can manage our db process for this consumer
+            return Task.CompletedTask;
+        }
+    }
+}
