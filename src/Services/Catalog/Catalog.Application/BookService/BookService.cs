5f1a28b22a04aa2879ab97c8b6631d2d394a503f
berkalgl <berkalgull@gmail.com>
2022-10-11 14:02:31 +0300

    Catalog application

---
+++ src/Services/Catalog/Catalog.Application/BookService/BookService.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.DataAccess.Repositories.Interfaces;
+using Catalog.Domains.Entities;
+using MassTransit;
+using MessagesAndEvents.Requests;
+
+namespace Catalog.Application.BookService
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public BookService(IBookRepository bookRepository, IPublishEndpoint publishEndpoint)
+        {
+            _bookRepository = bookRepository;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public bool AddBookToTheBasket(int id, int userId)
+        {
+            var addedBook = Get(id);
+
+            // async calling
+            //Rabbit MQ
+            AddBookToBasketRequest request = new AddBookToBasketRequest
+            {
+                BookId = addedBook.Id,
+                Description = addedBook.Description,
+                Name = addedBook.Name,
+                Price = addedBook.Price,
+                UserId = userId
+            };
+
+            _publishEndpoint.Publish(request);
+            return true;
+        }
+
+        public IList<Book> Get()
+        {
+            return _bookRepository.GetAll();
+        }
+
+        public Book Get(int id)
+        {
+            return _bookRepository.GetById(id);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.Application/BookService/IBookService.cs
+++ src/Services/Catalog/Catalog.Application/BookService/IBookService.cs
@@ -1,16 +1,18 @@
-﻿using Catalog.Domains;
+﻿using Catalog.Domains.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Catalog.Application
+namespace Catalog.Application.BookService
 {
     public interface IBookService
     {
         IList<Book> Get();
 
         Book Get(int id);
+
+        bool AddBookToTheBasket(int id, int userId);
     }
 }
