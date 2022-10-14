1a0ed7a11e391e83c9b55cf3ac98545ec533810b
berkalgl <berkalgull@gmail.com>
2022-10-14 20:12:16 +0300

    Mail Notification

---
+++ src/Services/Notification/Mail/MailNotification/Consumer/AddBookToBasketConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using MessagesAndEvents.Requests;
+using Microsoft.Extensions.Logging;
+
+namespace MailNotification.Consumer
+{
+    public class AddBookToBasketConsumer : IConsumer<AddBookToBasketRequest>
+    {
+        public Task Consume(ConsumeContext<AddBookToBasketRequest> context)
+        {
+            Console.WriteLine($"Sepete {context.Message.Name} isimli kitap eklendiğine dair mail gönderildi! ");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/Notification/Mail/MailNotification/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MailNotification;
+using MailNotification.Consumer;
+using MassTransit;
+
+var b = new B(1, 2);
+b.Writea();
+var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.Host("localhost", "/", host =>
+    {
+        host.Username("guest");
+        host.Password("guest");
+    });
+
+    cfg.ReceiveEndpoint("AddBookToBasketRequest", e =>
+    {
+        e.Consumer<AddBookToBasketConsumer>();
+    });
+});
+
+
+Console.WriteLine("Hello, World!");
+busControl.Start();
+Console.ReadLine();
