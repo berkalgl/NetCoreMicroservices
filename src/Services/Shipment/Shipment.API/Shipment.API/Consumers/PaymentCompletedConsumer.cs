95aba9b3c79e7a79c89f844df3672c45062359c4
berkalgl <berkalgull@gmail.com>
2022-10-25 09:14:30 +0300

    Shipment API

---
+++ src/Services/Shipment/Shipment.API/Shipment.API/Consumers/PaymentCompletedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using MessagesAndEvents.Events;
+
+namespace Shipment.API.Consumers
+{
+    public class PaymentCompletedConsumer : IConsumer<PaymentCompleted>
+    {
+        private readonly ILogger<PaymentCompletedConsumer> _logger;
+        public PaymentCompletedConsumer(ILogger<PaymentCompletedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<PaymentCompleted> context)
+        {
+            _logger.LogInformation($"Payment of {context.Message.OrderId} Completed In Shipment...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
